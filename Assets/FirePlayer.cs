85b9db2fc3a38cf737085a20ac8b80a021c534b8
monsefblzzz <monsefblouz@gmail.com>
2024-02-13 09:07:46 +0100

    Initial commit

---
+++ Assets/FirePlayer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePlayer : MonoBehaviour {
+    
+    public GameObject bullet;
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            GameObject g = (GameObject) Instantiate(bullet, 
+            transform.position, 
+            Quaternion.identity);
+
+            Physics2D.IgnoreCollision(g.GetComponent<Collider2D>(), 
+            transform.parent.GetComponent<Collider2D>());
+        }
+        
+    }
+    
+}
---
+++ Assets/Spawn.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+    
+    public GameObject ship;
+    
+    public float interval = 1;
+    
+    void Start() {
+    
+        InvokeRepeating("SpawnNext", interval, interval);
+        
+    }
+
+    void SpawnNext() {
+    
+        Instantiate(ship, transform.position, Quaternion.identity);
+        
+    }
+
+}
